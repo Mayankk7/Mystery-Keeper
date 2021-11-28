220d2dc667584266f954f655287591a50e02ddf2
Mayankk7 <mayank26032@gmail.com>
2021-11-28 16:31:28 +0530

    Updated UI . Removed Google Authentication

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # SecretKeeper
 A platform where one can post their secret anonymously.
-Uses local as well as google authentication to login into the server.
+
--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const encrypt = require("mongoose-encryption");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 const app=express();
 
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser : true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://User:User@cluster0.xgg1s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser : true, useUnifiedTopology: true})
+.then(()=> {console.log("Data base connected !!")})
+.catch((err) => {console.log(err)});
 
 mongoose.set("useCreateIndex",true);
-console.log(process.env.CLIENT_ID);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -60,33 +60,12 @@ passport.deserializeUser(function(id, done){
     });
 });
 
-passport.use(new GoogleStrategy({
-    clientID : process.env.CLIENT_ID,
-    clientSecret : process.env.CLIENT_SECRET,
-    callbackURL :  "http://localhost:3000/auth/google/secrets",
-    userProfileURL : "https://www.googleapis.com/oauth2/v3/userinfo"
-},
-function(accessToken, refreshToken, profile, cb){
-    User.findOrCreate({googleID : profile.id}, function(err, user){
-        return cb(err,user);
-    });
-}
-));
 
 app.get("/",function(req,res){
     res.render("home");
 })
 
-app.get("/auth/google", 
-    passport.authenticate("google", {scope : ["profile"] })
-);
 
-app.get("/auth/google/secrets",
-passport.authenticate("google", {failureRedirect : "/login"}),
-    function(req,res){
-        res.redirect("/secrets");
-    }
-);
 
 
 app.get("/login",function(req,res){
